25a7b8f4b5405a3f28bb7308a11fe78cdba5a960
Rishabhgoel21 <41298350+Rishabhgoel21@users.noreply.github.com>
2018-12-30 22:38:50 +0530

    jQuery Exercise

---
+++ 19_jQuery/Selectors/main.js
@@ -0,0 +1,11 @@
+//select all divs and assign them background purple
+$("div").css("background","purple");
+
+//select divs with highlight class and make size 200px
+$(".highlight").css("width","200px");
+
+//select divs with third id and background orange
+$("#third").css("border","5px dashed orange");
+
+//select only 1st div
+$("div:first-of-type").css("background","pink");
