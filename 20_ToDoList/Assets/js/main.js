f6ab8a06ead239491df3b82da4b4ff27b0eed43b
Rishabhgoel21 <41298350+Rishabhgoel21@users.noreply.github.com>
2019-01-02 18:33:58 +0530

    Project : To-Do List

---
+++ 20_ToDoList/Assets/js/main.js
@@ -0,0 +1,27 @@
+//line-through toDos when clicked
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
+
+//delete ToDos
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(600, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//adding a new ToDo
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        var toDoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>" + toDoText + "</li>");   
+    }
+});
+
+$(".fa-plus-circle").click(function () { 
+    $("input").fadeToggle(600,function(){
+        //transition done
+    });
+});
